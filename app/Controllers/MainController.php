4715da13947b1b3698f4abb2d3f284bf14c43a6b
chipslays <hello@chipslays.ru>
2021-02-12 04:24:09 +0400

    add http to controllers

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -4,12 +4,10 @@ namespace App\Controllers;
 
 use App\Core\Controller;
 use Container\Container as App;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public static function index()
     {
         App::render('pages/index');
     }
